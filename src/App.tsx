9379cad2e141e29c041deb47e3b36e9e14a158bf
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-02-28 21:03:54 -0500

    Attempting to implement automatic deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          Hello
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          Hello
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Trackify</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
