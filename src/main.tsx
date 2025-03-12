462985441cef9765d20d9f67084a3517e7a1ffa3
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-03-12 15:23:47 -0400

    Added layout and connected navbar via browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import Layout from './Layout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+		  <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+		  </Routes>
+	  </BrowserRouter>
   </StrictMode>,
 )
