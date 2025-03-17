bacce74df3bae0dc2229495a2d2d0230a29d7043
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-03-16 23:47:40 -0400

    Implemented log in with firebase using email and password

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../services/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 import "../styles/LogInForm.css";
 
 export default function LogInForm () {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
     return (
       <div className='LogInForm'>
           <h1>Log In</h1>
 
-          <form>
+          <form onSubmit={handleLogin}>
             {/* Email */}
             <div className="login-email-container">
                 <label htmlFor="email">Email</label>
@@ -13,6 +33,7 @@ export default function LogInForm () {
                     name="email" 
                     type="email"
                     placeholder="johnsmith@example.com"
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 ></input>
             </div>
@@ -24,12 +45,15 @@ export default function LogInForm () {
                     type="password" 
                     name="password" 
                     placeholder="Enter your password"
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                 ></input>
             </div>
 
-            <button className="login-form-btn">Log In</button>
+            <button className="login-form-btn" type="submit">Log In</button>
           </form>
+
+          {error && <p>Error: {error}</p>}
 
           <div className="login-github-container">
             <h3>Log in with</h3>
