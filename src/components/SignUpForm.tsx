81228541ad03aa13c80f518ecac0b5544c23303b
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-03-16 23:07:11 -0400

    Added and styled sign up form

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -3,7 +3,49 @@ import "../styles/SignUpForm.css";
 export default function SignUpForm () {
     return (
       <div className='SignUpForm'>
-          Sign Up form
+          <h1>Sign Up</h1>
+
+          <form>
+            {/* Email */}
+            <div className="login-email-container">
+              <label htmlFor="email">Email</label>
+                <input className="log-in-input-field" 
+                name="email" 
+                type="email"
+                placeholder="johnsmith@example.com"
+                required
+              ></input>
+            </div>
+
+          {/* Password */}
+          <div className="login-password-container">
+            <label htmlFor="password">Password</label>
+            <input className="log-in-input-field"
+            type="password" 
+            name="password" 
+            placeholder="Enter your password"
+            required
+            ></input>
+          </div>
+
+          {/* Password confirmation */}
+          <div className="login-password-container">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input className="log-in-input-field"
+            type="password" 
+            name="confirm-password" 
+            placeholder="Enter your password"
+            required
+            ></input>
+          </div>
+
+        <button className="login-form-btn">Sign Up</button>
+      </form>
+
+      <div className="login-github-container">
+        <h3>Sign up with</h3>
+        <img className="github-logo-login" src="github-brands-solid.svg" />
       </div>
-    );
+    </div>
+  );
 }
