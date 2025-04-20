fa3aadcc89c65f9aa25925a0a77d0bc1c5076824
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2025-04-20 13:45:30 -0400

    Changed path for homepage

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
 		  <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<LogInSignUpPage />} />
         </Route>
 		  </Routes>
